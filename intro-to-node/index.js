1a6f116abf4ae7fd0fd75071adce97e3929914c4
Rene Ouoba <55203223+rouoba@users.noreply.github.com>
2020-09-13 12:46:30 -0500

    Introduction to Node.js

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
+var mySuperHeroName = superheroes.random();
+var myVillainName = supervillains.random();
+
+console.log(myVillainName);
