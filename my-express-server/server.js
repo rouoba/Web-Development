ceed7e6920d8a9f841769cd091bdef4dd96f4986
Rene Ouoba <rene@Renes-MacBook-Pro.local>
2020-09-17 23:47:02 -0500

    added heroku hosting to some projects

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,5 +1,12 @@
-const express = require("express");
+const express = require("express"); 
 
 const app = express();
 
-app.listen(3000);
+app.get("/", function(request, response) {   //what happens when a browser makes a get request to the home directory (root "/")
+    // console.log(request);
+    response.send("Hi");
+})   
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
